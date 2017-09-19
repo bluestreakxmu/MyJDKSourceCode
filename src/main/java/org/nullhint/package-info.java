edf2571f49c9d3687d25f6f1067fc65df98c1541
lixibo <alanlxb.xmu@gmail.com>
2017-09-19 21:01:02 +0800

    update package-info.java

--- src/main/java/org/nullhint/package-info.java
+++ src/main/java/org/nullhint/package-info.java
@@ -1,5 +1,5 @@
 /**
  * 重新实现JDK源码的重要部分。<br>
- * 所有的JDK源码包都放在“io.lixibo”这个包下，并且源码加上类名加上“My”，比如HashMap变成MyHashMap。
+ * 所有的JDK源码包都放在“org.nullhint”这个包下，并且源码加上类名加上“My”，比如HashMap变成MyHashMap。
  */
 package org.nullhint;
